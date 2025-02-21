a1384d4cd9d79982eed08eb97c4d2a7c2fa81a8b
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-02-21 16:13:22 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
-// import ProductDetails from "./components/product-details";
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
@@ -32,7 +32,7 @@ const ProductPage = async ({ params }: ProductPageProps) => {
   return (
     <div className="flex h-full flex-col">
       <ProductHeader product={product} />
-      {/* <ProductDetails product={product} /> */}
+      <ProductDetails product={product} />
     </div>
   );
 };
